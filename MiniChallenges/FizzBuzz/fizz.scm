238d561ace0564152ddeec49fe3a6b516717e7cf
Jason B <bjason@gmail.com>
2015-07-13 15:26:16 -0700

    Fizz buzz complete.

--- MiniChallenges/FizzBuzz/fizz.scm
+++ MiniChallenges/FizzBuzz/fizz.scm
@@ -2,13 +2,12 @@
 
  (define (miniloop curr)
    (define x curr)
-   (print curr)
    (cond 
          ( (> x  99)     )
-         ( (&&  (= (modulo x 5)  0) (= (modulo x 3)  )   (print "FizzBuzz") (miniloop (+ x 1))   )  
+         ( (and (= (modulo x 5)  0)  (= (modulo x 3)  0) )   (print "FizzBuzz") (miniloop (+ x 1))   )  
          ( (= (modulo x 3)  0)  (print "Fizz") (miniloop (+ x 1))   )  
          ( (= (modulo x 5)  0)  (print "Buzz") (miniloop (+ x 1))   )  
-         (else (miniloop  (+ x 1 ) ) )
+         (else  (print x) (miniloop  (+ x 1 ) ) )
          )
    )
 
