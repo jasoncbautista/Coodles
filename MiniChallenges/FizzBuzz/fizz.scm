80921310620e114505b1d5346e4622b29875700b
Jason B <bjason@gmail.com>
2015-07-13 15:24:07 -0700

    Fizz printout

--- MiniChallenges/FizzBuzz/fizz.scm
+++ MiniChallenges/FizzBuzz/fizz.scm
@@ -5,7 +5,9 @@
    (print curr)
    (cond 
          ( (> x  99)     )
+         ( (&&  (= (modulo x 5)  0) (= (modulo x 3)  )   (print "FizzBuzz") (miniloop (+ x 1))   )  
          ( (= (modulo x 3)  0)  (print "Fizz") (miniloop (+ x 1))   )  
+         ( (= (modulo x 5)  0)  (print "Buzz") (miniloop (+ x 1))   )  
          (else (miniloop  (+ x 1 ) ) )
          )
    )
